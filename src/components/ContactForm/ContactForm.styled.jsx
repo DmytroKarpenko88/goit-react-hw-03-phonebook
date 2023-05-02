95fef3511092b48396a2fc0e322419a6d2df1371
Dmytro Karpenko <kardmitriy@gmail.com>
2023-05-02 20:37:29 +0300

    update

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -45,8 +45,7 @@ export const Button = styled.button`
   color: #606060;
   text-shadow: 0 1px #fff;
 
-  &:hover,
-  &:focus {
+  &:hover {
     border-color: #007bff;
     box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
   }
--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -22,8 +22,7 @@ export const Button = styled.button`
 
   border-radius: 4px;
 
-  &:hover,
-  &:focus {
+  &:hover {
     border-color: #007bff;
     box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
   }
